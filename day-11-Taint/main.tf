64dd7d788fe18ef189a8a04b712a948c993864f2
Prajwal Gawande <prajwalgawande1311@gmail.com>
2025-11-23 11:58:47 +0530

    Day-11: Taint – Demonstrated marking resources for forced recreation using 'terraform taint' and 'terraform untaint'. Used for fixing unhealthy resources by destroying and recreating them.

---
+++ day-11-Taint/main.tf
@@ -0,0 +1,104 @@
+# 
+provider "aws" {
+  
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.name.id
+  tags = {
+    Name = "internet-gateway"
+  }
+}
+
+# Route Table for Public Subnet
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.name.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "Public-RT"
+  }
+}
+
+# Associate the Route Table with your subnet
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_vpc" "name" {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+      Name = "TF-VPC-1"
+    }
+}
+
+resource "aws_subnet" "public" {
+    vpc_id = aws_vpc.name.id
+    cidr_block = "10.0.1.0/24"
+    availability_zone = "us-west-2a"
+    tags = {
+      Name = "Public-Subnet-1"
+    }
+}
+
+resource "aws_security_group" "name" {
+    name = "EC2-Connection"
+    description = "Allow SSH Connection"
+    vpc_id = aws_vpc.name.id
+    tags = {
+      Name = "SSH"
+    }
+    ingress{
+        description = "SSH for all"
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress{
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+  
+}
+resource "aws_instance" "name" {
+    ami = "ami-04f9aa2b7c7091927"
+    instance_type = "t3.micro"
+    subnet_id = aws_subnet.public.id
+    key_name = "first"
+    tags = {
+        Name = "taint-instance"
+    }
+     vpc_security_group_ids = [aws_security_group.name.id]
+    associate_public_ip_address = true
+}
+
+
+resource "null_resource" "run_script" {
+
+    provisioner "remote-exec" {
+        connection {
+          host = aws_instance.name.public_ip
+          user = "ec2-user"
+          private_key = file("C:\\Users\\Prajawal\\.ssh\\first.pem")
+        }
+
+        inline = [ 
+            "echo Hello from local something change >> /tmp/file.txt"
+         ]
+      
+    }
+}
+
+#   provisioner "local-exec" {
+#     command = "echo instance ${self.id} created >> main.txt"
+#   }
+# }
